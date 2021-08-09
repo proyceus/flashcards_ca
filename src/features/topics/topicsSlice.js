14957bcdc1374b36c8355d1b7e620ce4331cfabe
Pierre <proyceus@gmail.com>
2021-08-09 13:33:09 -0400

    First Commit

---
+++ src/features/topics/topicsSlice.js
@@ -0,0 +1,22 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export const topicsSlice = createSlice({
+  name: "topics",
+  initialState: {
+    topics: {}
+  },
+  extraReducers: {
+    [addTopic]: (state, action) => {
+      state.topics[action.payload.id] = {
+        name: action.payload.name,
+        icon: action.payload.icon,
+        quizIds: []
+      };
+    }
+  }
+});
+
+export const selectTopics = (state) => {
+  state.topics;
+};
+export default topicsSlice.reducer;
